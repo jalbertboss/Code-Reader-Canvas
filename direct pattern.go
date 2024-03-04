ab47face84173f3101794adaf2077a88a864d41c
Anna Fischer-Fernandez <161723657+jalbertboss@users.noreply.github.com>
2024-03-04 22:04:04 +0300

    Update direct pattern.go

--- direct pattern.go
+++ direct pattern.go
@@ -8,3 +8,14 @@ func NewSpamMigitationAnteDecorator(pylonsmodulekeeper PylonsKeeper) AnteSpamMig
 			if AccountTrack[addr.String()] > maxTxs {
 				panic(fmt.Sprintf("maximum txs in block is %d ", maxTxs))
 			}
+
+
+			func DefaultConfig() network.Config {
+	encCfg := MakeEncodingConfig()
+
+	genState := ModuleBasics.DefaultGenesis(encCfg.Codec)
+	genState["pylons"] = CustomGenesisHelper(encCfg.Codec)
+
+	return network.Config{
+		Codec:             encCfg.Codec,
+		TxConfig:          encCfg.TxConfig,
